b2c268e4b9084824175c8e01c5813c810b6c8f5f
Guillaume Gaisné <guillaume.gaisne01@gmail.com>
2023-02-11 17:17:02 +0100

    Création du modèle Consultation
    Migration de la base de données

--- cabinet/secretariat/models.py
+++ cabinet/secretariat/models.py
@@ -6,3 +6,16 @@ class Patient(models.Model):
     last_name = models.CharField(max_length=100)
     adress = models.CharField(max_length=500)
     email = models.EmailField()
+
+class Consultation(models.Model):
+
+    class Type(models.TextChoices):
+        VISITE = 'visite'
+        SUIVI = 'suivi'
+        OPERATION = 'operation'
+
+    date = models.DateField()
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE, related_name="patient")
+    nom = models.CharField(max_length=100)
+    description = models.TextField()
+    type = models.CharField(choices=Type.choices, max_length=10)
