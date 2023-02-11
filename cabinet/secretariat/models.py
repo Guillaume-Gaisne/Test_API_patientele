23723aa4a396c916874ef05b09cdbfc5a298a5a2
Guillaume Gaisné <guillaume.gaisne01@gmail.com>
2023-02-11 12:48:19 +0100

    Création du modèle Patient
    Migration de la base de données
    Ajout de deux entrées Patient dans le shell

--- cabinet/secretariat/models.py
+++ cabinet/secretariat/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Patient(models.Model):
+
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    adress = models.CharField(max_length=500)
+    email = models.EmailField()
