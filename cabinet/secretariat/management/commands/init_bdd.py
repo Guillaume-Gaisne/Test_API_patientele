aaa2e7430b43b9a696acb466573699ad839d5215
Guillaume Gaisné <guillaume.gaisne01@gmail.com>
2023-02-12 11:58:44 +0100

    Ajout de conditions sur la création d'adresse mail
    -Gestion des accents dans les prénoms, noms
    -Gestion des espaces pour les noms composés
    -Réinitialisation de la BDD à chaque appel de la commande

--- cabinet/secretariat/management/commands/init_bdd.py
+++ cabinet/secretariat/management/commands/init_bdd.py
@@ -2,13 +2,14 @@ from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 
 from faker import Faker
+from unidecode import unidecode
 
 from secretariat.models import Patient
 
 ADMIN_LOGIN = 'admin'
 PASSWORD_LOGIN = '123'
 
-NB_ENTRIES = 5
+NB_ENTRIES = 10
 CHOICE_NATIONALITY = 'fr_FR'
 
 class Command(BaseCommand):
@@ -18,14 +19,24 @@ class Command(BaseCommand):
         User.objects.all().delete()
         User.objects.create_superuser(username=ADMIN_LOGIN, password=PASSWORD_LOGIN)
 
-
         fake_patient = Faker(CHOICE_NATIONALITY)
 
-        for _ in range(NB_ENTRIES):
+        Patient.objects.all().delete()
+
+        for num_patient in range(NB_ENTRIES):
             first_name, last_name = fake_patient.name().split(' ', maxsplit=1)
-            email = '.'.join((first_name, last_name)) + '@mail.xyz'
-            email = email.replace(' ', '_')
+
+            tmp_email = '.'.join((first_name, last_name)) + '@mail.xyz'
+            # To ensure the unicity of the mail address, we had a number to the last name
+            if Patient.objects.filter(email=tmp_email).exists():
+                email = '.'.join((first_name, last_name + f'{num_patient}')) + '@mail.xyz'
+            else:
+                email = tmp_email   
+            # To deal with accents and spaces in composed last names
+            email = unidecode(email.replace(' ', '_'))
+
             address = fake_patient.address()
+
             Patient.objects.create(
                 first_name=first_name,
                 last_name=last_name,
