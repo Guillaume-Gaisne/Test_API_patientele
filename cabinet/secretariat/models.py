45061b34fccb7e5acc5994899179b0dff9e2cb85
Guillaume Gaisné <guillaume.gaisne01@gmail.com>
2023-02-12 17:23:17 +0100

    Test d'encryptage avec django_cryptography
    -Une erreur est renvoyée pour l'encryptage du EmailField.
    -L'encryptage pour les CharField semble marcher.

--- cabinet/secretariat/models.py
+++ cabinet/secretariat/models.py
@@ -1,10 +1,21 @@
 from django.db import models
+from django_cryptography.fields import encrypt
 
 class Patient(models.Model):
 
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    address = models.CharField(max_length=500)
+    first_name = encrypt(models.CharField(max_length=100))
+    last_name = encrypt(models.CharField(max_length=100))
+    address = encrypt(models.CharField(max_length=500))
+    # With this method of encryption, this error is raised by the shell when trying to encrypt
+    # the EmailField:
+
+    # django.core.exceptions.FieldError: Unsupported lookup 'exact' for EncryptedEmailField 
+    # or join on the field not permitted, perhaps you meant exact or iexact?
+    
+    # Apparently, people had similar issues in the past but with the CharField
+    # and I saw it was intended, see the Github issue below
+    # https://github.com/georgemarshall/django-cryptography/issues/84
+    # However, now the CharField is encryptable
     email = models.EmailField()
 
 class Consultation(models.Model):
